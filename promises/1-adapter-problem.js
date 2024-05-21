89469ce697f39fbf7eadb9a916846a881ba7c7c8
Andrey Chiglintcew <melanj.ks.ua@gmail.com>
2024-05-21 06:43:04 +0300

    Promise Task-1 solution: Promisify Adapter for cb, propagate the error

--- promises/1-adapter-problem.js
+++ promises/1-adapter-problem.js
@@ -3,6 +3,7 @@
 // Task: create Promise-returning adapter function `totalAsync`
 // Do not change function `total` contract, just call `total` from
 // `totalAsync` and convert contracts
+
 // Should not be changed
 const total = (items, callback) => {
   let result = 0;
@@ -16,16 +17,35 @@ const total = (items, callback) => {
   callback(null, result);
 };
 
+const totalAsync = (items) => {
+  return new Promise((resolve, reject) => {
+    (total(items, (error, money) => {
+      if (error) return reject(error);
+      else return resolve(money);
+    }));
+  });
+};
 // const totalAsync = (items) => new Promise...
 
 const electronics = [
   { name: 'Laptop', price: 1500 },
   { name: 'Keyboard', price: 100 },
-  { name: 'HDMI cable', price: 10 },
+  { name: 'HDMI cable', price: 10 }
 ];
 
 // Also rewrite call, use .then instead of callback
-total(electronics, (error, money) => {
-  if (error) console.error({ error });
-  else console.log({ money });
-});
+// total(electronics, (error, money) => {
+//   if (error) console.error({ error });
+//   else console.log({ money });
+// });
+
+(async () => {
+  const money = await totalAsync(electronics).catch(error => {
+    console.error({ error });
+    // throw error; // Re-throw the error if you need to propagate it further
+  });
+  console.log({ money });
+})();
+
+
+
