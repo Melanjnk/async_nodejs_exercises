e18fe3511633f35fbca4737dad9bdc90efd87b2c
Andrey Chiglintcew <melanj.ks.ua@gmail.com>
2024-05-21 06:55:38 +0300

    Promise Task-2-solution: cb to Promise

--- promises/2-promise-problem.js
+++ promises/2-promise-problem.js
@@ -3,25 +3,25 @@
 // Task: rewrite `total` from callbacks contract to promises
 // Hint: do not use `async () =>` syntax
 
-const total = (items, callback) => {
-  let result = 0;
-  for (const item of items) {
-    if (item.price < 0) {
-      callback(new Error('Negative price is not allowed'));
-      return;
+const total = (items) => {
+  return new Promise((resolve, reject) => {
+    let result = 0;
+    for (const item of items) {
+      if (item.price < 0) {
+        reject(new Error('Negative price is not allowed'));
+      }
+      result += item.price;
     }
-    result += item.price;
-  }
-  callback(null, result);
+    resolve(result);
+  });
 };
 
 const electronics = [
   { name: 'Laptop', price: 1500 },
   { name: 'Keyboard', price: 100 },
-  { name: 'HDMI cable', price: 10 },
+  { name: 'HDMI cable', price: 10 }
 ];
 
-total(electronics, (error, money) => {
-  if (error) console.error({ error });
-  else console.log({ money });
-});
+total(electronics)
+  .then(data => console.log({ data }))
+  .catch( e => console.error(e));
