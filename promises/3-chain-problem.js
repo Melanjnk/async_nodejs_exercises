040337992f6a4f26d253ff587b34426c97f73c43
Andrey Chiglintcew <melanj.ks.ua@gmail.com>
2024-05-21 07:18:42 +0300

    Promise Task-3-solution: Chaining

--- promises/3-chain-problem.js
+++ promises/3-chain-problem.js
@@ -11,11 +11,13 @@
 const CURRENCY_RATE = 1.09;
 
 const convert = ({ price }) => Promise.resolve(price * CURRENCY_RATE);
+// for test reject & catch
+// const convert = ({ price }) => Promise.reject(price * CURRENCY_RATE);
 
 const electronics = [
   { name: 'Laptop', price: 1500 },
   { name: 'Keyboard', price: 100 },
-  { name: 'HDMI cable', price: 10 },
+  { name: 'HDMI cable', price: 10 }
 ];
 
 const prices = electronics.map(convert);
@@ -25,3 +27,9 @@ const formatMoney = (x) => parseFloat(x.toFixed(2));
 const sum = (a, b) => a + b;
 
 // Write solution below
+
+Promise.all(prices)
+  .then(data => data.reduce(sum))
+  .catch(error => console.error({ 'error': error }))
+  .then(total => console.log(`Total[${CURRENCY_RATE}]: ${formatMoney(total)}`))
+  .finally(() => console.log('All done'));
